e5246d17d356a9a9cadea0e1300914e66d386a26
yangg0228 <yangg0228@gmail.com>
2020-05-08 22:56:47 +0900

    week2

---
+++ iOS_Week2_Seminar/iOS_Week2_Seminar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Week2_Seminar
+//
+//  Created by 양지영 on 2020/05/01.
+//  Copyright © 2020 양지영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
